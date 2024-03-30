1e4c87d8f665c9514e5c94ae27683a4980656cd5
Tamhi <hirahudakhan20@gmail.com>
2024-03-30 21:09:44 +0530

    added top 5 campaign un desc order of raisedAmt

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { database } from "../utils/firebase";
 import { INRFormat } from "../utils/rupees_format";
 import { IoMdTrendingUp } from "react-icons/io";
 import {
@@ -148,182 +151,55 @@ const HomePage = () => {
     // console.log(recentDonations);
   }, []);
 
+
+  //fetching data
+  const data = useState(null);
+  const campaignsRef = collection(database, "campaigns");
+  const [campaigns, setCampaigns] = useState([]);
+  async function getCampaigns() {
+    const data = await getDocs(campaignsRef);
+    setCampaigns(data.docs);
+  }
+  useEffect(() => {
+    getCampaigns();
+  }, []);
+
   return (
-    <section className="px-2 py-2 space-y-6">
-      <div className="flex bg-blue-100 p-2 gap-2 rounded-lg">
-        {["weekly", "monthly", "yearly"].map((filter, index) => {
-          return (
-            <button
-              className={`px-4 py-1 rounded capitalize ${
-                filter == dataFilter && "bg-white text-blue-500"
-              }`}
-              onClick={() => setDataFilter(filter)}
-              key={index}
-            >
-              {filter}
-            </button>
-          );
-        })}
+    <>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr className="text-base">
+              <th></th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Raised Amount</th>
+              <th>Goal Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {campaigns
+              .sort((a, b) => b.data().raisedAmount - a.data().raisedAmount) 
+              .slice(0, 5)
+              .map((campaign, index) => {
+                const campaignData = campaign.data(); 
+                return (
+                  <tr key={index} className="hover:bg-blue-200">
+                    <th>{index + 1}</th>
+                    <td>{campaignData.title}</td>
+                    <td>{campaignData.category}</td>
+                    <td>{campaignData.raisedAmount}</td>
+                    <td>{campaignData.goalAmount}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
       </div>
-
-      <div className="grid3">
-        {amountStats.map((data, index) => {
-          if (index > 2)
-            return (
-              <div className="p-4 bg-white rounded-md" key={index}>
-                <Pie data={data} />
-              </div>
-            );
-
-          return (
-            <div
-              className="grid content-between p-4 bg-white rounded-md"
-              key={index}
-            >
-              <div className="space-y-4">
-                <div className="text-slate-500">{data.title}</div>
-                <div className="font-[600]">{INRFormat(data.value)}</div>
-              </div>
-              <Bar data={data.data} options={options} />
-            </div>
-          );
-        })}
-      </div>
-      {/* <div className="grid3">
-        {amountData ? (
-          <h1>Loading</h1>
-        ) : (
-          transactionStats.map((data, index) => {
-            if (index > 2)
-              return (
-                <div className="p-4 bg-white rounded-md">
-                  <Pie data={data} />
-                </div>
-              );
-
-            return (
-              <div
-                className="grid content-between p-4 bg-white rounded-md"
-                key={index}
-              >
-                <div className="space-y-4">
-                  <div className="text-slate-500">{data.title}</div>
-                  <div className="font-[600]">{INRFormat(data.value)}</div>
-                </div>
-                <Bar data={data.data} />
-              </div>
-            );
-          })
-        )}
-      </div> */}
-
-      <div className="bg-white p-4 rounded-md overflow-hidden">
-        <div className="flex items-center gap-4 text-xl text-slate-500 font-[600] mb-4">
-          <IoMdTrendingUp size={24} /> Trending campaigns
-        </div>
-        <div className="overflow-auto w-[calc(100vw-48px)] lg:w-[calc(100vw-304px)]">
-          <table className="table whitespace-nowrap">
-            <thead>
-              <tr>
-                <th className="">S. No.</th>
-                <th className="">Fundraisers</th>
-                <th className="">Created on</th>
-                <th className="">Expired by</th>
-                <th className="">Goal amount</th>
-                <th className="">Raised amount</th>
-                <th className="">Tip amount</th>
-                <th className="">Total collection</th>
-              </tr>
-            </thead>
-            <tbody>
-              {!trendingData ? (
-                <tr>
-                  <td>Loading...</td>
-                </tr>
-              ) : (
-                trendingData?.map((data, index) => {
-                  const url = `https://give-umma.vercel.app/details/${data.id}`;
-                  const createdOn = moment(data.data().date).format(
-                    "DD-MM-YYYY"
-                  );
-                  const expiredBy = moment(data.data().date)
-                    .add(2, "month")
-                    .format("DD-MM-YYYY");
-
-                  const raisedAmount = data.data()?.raisedAmount ?? 0;
-                  const goalAmount = data.data()?.goalAmount ?? 0;
-                  const tipAmount = data.data()?.totalTip ?? 0;
-                  return (
-                    <tr key={index}>
-                      <td className="">{index + 1}</td>
-                      <td className="">
-                        <a href={url} target="_blank">
-                          {url}
-                        </a>
-                      </td>
-                      <td className="">{createdOn}</td>
-                      <td className="">{expiredBy}</td>
-                      <td className="">{INRFormat(goalAmount)}</td>
-                      <td className="">{INRFormat(raisedAmount)}</td>
-                      <td className="">{INRFormat(tipAmount)}</td>
-                      <td className="">
-                        {INRFormat(raisedAmount + tipAmount)}
-                      </td>
-                    </tr>
-                  );
-                })
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div className="bg-white p-4 rounded-md overflow-hidden">
-        <div className="flex items-center gap-4 text-xl text-slate-500 font-[600] mb-4">
-          <BsClockHistory size={24} /> Recent donations
-        </div>
-        <div className="overflow-auto w-[calc(100vw-48px)] lg:w-[calc(100vw-304px)]">
-          <table className="table whitespace-nowrap">
-            <thead>
-              <tr>
-                <th className="">S. No.</th>
-                <th className="">Name</th>
-                <th className="">Amount</th>
-                <th className="">Tip</th>
-                <th className="">Email</th>
-                <th className="">Mobile</th>
-              </tr>
-            </thead>
-            <tbody>
-              {!recentDonations ? (
-                <tr>
-                  <td>Loading...</td>
-                </tr>
-              ) : (
-                recentDonations?.map((data, index) => {
-                  const name = data.data()?.name ?? 0;
-                  const amount = data.data()?.amount ?? 0;
-                  const tip = data.data()?.tip ?? 0;
-                  const email = data.data()?.email ?? 0;
-                  const mobile = data.data()?.mobile ?? 0;
-                  return (
-                    <tr key={index}>
-                      <td className="">{index + 1}</td>
-                      <td className="">{name}</td>
-                      <td className="">{INRFormat(amount)}</td>
-                      <td className="">{INRFormat(tip)}</td>
-                      <td className="">{email}</td>
-                      <td className="">{mobile}</td>
-                      
-                    </tr>
-                  );
-                })
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </section>
+    </>
   );
+  
+  
 };
 
 export default HomePage;
